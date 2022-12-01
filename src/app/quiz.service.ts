186adc59a979d7f1d455666399d608d89c9cd2c3
Tom Steele <tsteele@madisoncollege.edu>
2022-12-01 08:15:00 -0600

    saveQuizzes for #week-14...

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,13 +1,23 @@
-import { HttpClient } from '@angular/common/http';  
 import { Injectable } from '@angular/core';
-import { lastValueFrom} from 'rxjs'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { repeat, lastValueFrom } from 'rxjs';
 
 interface QuizFromWeb {
+  name: string;
+  questions: {
     name: string;
-    questions: {
-      name: string;
-    }[];
-  }
+  }[];
+}
+
+export interface ShapeForSavingEditedQuizzes {
+  quiz: string;
+  questions: { question: string; }[];
+}
+​
+export interface ShapeForSavingNewQuizzes {
+  quizName: string;
+  quizQuestions: string[];
+}
 
 @Injectable({
   providedIn: 'root'
@@ -15,27 +25,63 @@ interface QuizFromWeb {
 export class QuizService {
 
   constructor(
-    private angulatHttpClient: HttpClient
+    private angularHttpSvc: HttpClient
   ) { }
 
   loadQuizzes = () => {
-    const quizzesFromWeb = lastValueFrom (
-        this.angulatHttpClient.get<QuizFromWeb[]>(
-        "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
-    )
+
+    const quizzesFromWeb = this.angularHttpSvc.get<QuizFromWeb[]>(
+      "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
     );
-    return quizzesFromWeb;
+
+    return lastValueFrom(
+      quizzesFromWeb.pipe(repeat(1))
+    );
   };
 
-  getMagicNumber = (callerWantsToSucceed:boolean): Promise<number> => {
+  getMagicNumber = (callerWantsToSucceed: boolean): Promise<number> => {
     return new Promise<number>(
-        (resolve, reject) => {
-            if (callerWantsToSucceed) {
-                resolve(42);
-            } else {
-                reject("Error!");
-            }
+      (resolve, reject) => {
+
+        //
+        // Some fancy long running code here...
+        //
+
+        // Ultimately resolve if successful.
+        if (callerWantsToSucceed) {
+          resolve(42);
         }
+        // Or reject if failure.
+        else {
+          reject("Promise was rejected ! ! !");
+        }
+      }
     );
-};
-}   
+  };  
+
+  saveQuizzes = (
+    changedQuizzes: ShapeForSavingEditedQuizzes[]
+    , newQuizzes: ShapeForSavingNewQuizzes[] = []
+  ) => {
+
+    let h = new HttpHeaders({
+      'Content-Type': 'application/json'
+      , 'X-Sas-Token': 'sig=K2WE6NQPtyoV6ke5hwPEaEaW52fgvyFWUeCEdPJls1s'
+    });
+
+    return lastValueFrom(
+      this.angularHttpSvc.post(
+        'https://modern-js.azurewebsites.net/save-quizzes-proxy'
+        , JSON.stringify(
+          {
+            "changedQuizzes": changedQuizzes
+            , "newQuizzes": newQuizzes
+          }
+        )
+        , {
+          headers: h
+        }
+      )
+    );
+  };  
+}
